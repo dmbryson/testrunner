81597af1a9d90ba6e140556987436e80ffbc8e4e
David M. Bryson <david@programerror.com>
2007-03-06 03:03:46 +0000

    Add test code and build files.

---
+++ main.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+
+const char* df1str = "data file test: ";
+const char* df2str = "another data file: ";
+const char ds2offset = 3;
+const char ds3offset = 3;
+const char ds4offset = 3;
+
+int main(int argc, const char* argv[])
+{
+  if (argc < 2) return -1;
+  
+  std::ofstream fp;
+
+  switch (argv[1][0]) {
+  case 'a':
+  case 'b':
+  case 'c':
+    fp.open("outfile.dat");
+    fp << df1str << argv[1][0] << std::endl;
+    fp.close();
+    break;
+  case 'd':
+    fp.open("outfile.dat");
+    fp << df1str << (argv[1][0] - ds2offset) << std::endl;
+    fp.close();
+  case 'e':
+    fp.open("outfile.dat", std::ios_base::app);
+    fp << df1str << (argv[1][0] - ds3offset) << std::endl;
+    fp.close();
+  case 'f':
+    fp.open("outfile.dat", std::ios_base::app);
+    fp << df1str << (argv[1][0] - ds4offset) << std::endl;
+    fp.close();
+    fp.open("outfile2.dat");
+    fp << df2str << argv[1][0] << std::endl;
+    fp.close();
+    break;
+  default:
+    return -2;
+  }
+
+  return 0;
+}
+
