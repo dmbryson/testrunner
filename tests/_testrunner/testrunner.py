b43333b0fa513377b882f4eb436675480a71819d
David M. Bryson <david@programerror.com>
2007-03-09 15:02:28 +0000

    test adding with abspath

--- tests/_testrunner/testrunner.py
+++ tests/_testrunner/testrunner.py
@@ -11,6 +11,7 @@ import string
 import sys
 import tempfile
 import time
+import xml.dom.minidom
 
 import pprint
 
@@ -25,6 +26,7 @@ class cTest:
   NOTFOUND = "file not found"
   DONOTMATCH = "content does not match"
   
+
   # cTest::cTest(string name, string tdir) {
   def __init__(self, name, tdir):
     global settings
@@ -48,28 +50,37 @@ class cTest:
   # } // End of cTest::cTest()
     
     
+
   # string cTest::getConfig(string sect, string opt, string default)
   def getConfig(self, sect, opt, default):
+    global settings
     try:
-      return self.cfg.get(sect, opt)
+      return self.cfg.get(sect, opt, False, settings)
     except:
       return default
   # } // End of cTest::getConfig()
   
 
+
   # string cTest::getSetting(string sect, string opt) {
   def getSetting(self, sect, opt):
     global settings
     try:
-      return self.cfg.get(sect, opt)
+      return self.cfg.get(sect, opt, False, settings)
     except:
       return settings[opt]
   # } // End of cTest::getSetting()
   
   
+
   # void cTest::runTest() {
   def runTest(self):
-    global tmpdir
+    global settings, tmpdir
+    
+    # If no expected results exist and in slave mode, or in master mode and
+    # subversion usage has been disabled then skip execution
+    if not self.has_expected and (settings["mode"] == "slave" or \
+      (settings["mode"] == "master" and settings.has_key("disable-svn"))): return
     
     confdir = os.path.join(self.tdir, "config")
     rundir = os.path.join(tmpdir, self.name)
@@ -98,23 +109,16 @@ class cTest:
     # Build dictionary of config structure
     confstruct = {}
     for root, dirs, files in os.walk(confdir):
-      if ".svn" in dirs: dirs.remove(".svn")
+      if settings["svnmetadir"] in dirs: dirs.remove(settings["svnmetadir"])
       for file in files:
         path = os.path.abspath(os.path.join(root, file))
         key = path[len(confdir) + 1:] # remove confdir from path
         confstruct[key] = path
         
       
-    # If no expected results exist, copy the results of the command as the new expected results
-    if not self.has_expected:
-      shutil.copytree(rundir, expectdir)
-      for cfile in confstruct.keys():
-        try:
-          os.remove(os.path.join(expectdir, cfile))
-        except OSError, e:
-          print "Warning: failed to remove conf file (%s) from expected" % cfile
-          print "  -- root cause: %s" % e
-      shutil.rmtree(rundir, True) # Clean up test directory
+    # If no expected results exist, defer processing of new expected results to results phase
+    if not self.has_expected: 
+      self.confstruct = confstruct
       return
 
     # Build dicitonary of expected structure
@@ -169,19 +173,76 @@ class cTest:
     # Clean up test directory
     shutil.rmtree(rundir, True)
   # } // End of cTest::runTest()
-      
+  
+  
+  
+  # string cTest::getRepositoryPath(string ) {
+  def getRepositoryPath(self):
+    global settings
+    
+    ifp = popen("cd %s; %s info --xml" % (settings["testdir"], settings["svnpath"]))
+    doc = xml.dom.minidom.parse(ifp)
+    if doc.documentElement.tagName != "info": return ""
+    
+    urltags = doc.getElementsByTagName("url")
+    if len(urltags) < 1 or urltags[0].firstChild.nodeType != urltags[0].firstChild.TEXT_NODE: return ""
+    return urltags[0].firstChild.data
+  # } // End of cTest::getRepositoryPath()
+  
+
+
+  # bool cTest::handleNewExpected() {
+  def handleNewExpected(self):
+    global settings
+
+    rundir = os.path.join(tmpdir, self.name)
+    expectdir = os.path.join(self.tdir, "expected")
+    
+    svn = settings["svnpath"]
+
+    if settings["mode"] == "master":
+      if settings.has_key("disable-svn"): return True
+      svndir = os.path.join(tmpdir, "_svn_tests")
+      if not os.path.exists(svndir):
+        ecode = os.spawnlp(os.P_WAIT, svn, svn, "checkout", getRepositoryPath(), svndir)
+        if ecode != 0: return False
+      expectdir = os.path.join(svndir, self.name, "expected")
+
+    shutil.copytree(rundir, expectdir)
+    for cfile in self.confstruct.keys():
+      try:
+        os.remove(os.path.join(expectdir, cfile))
+      except OSError, e:
+        print "Warning: failed to remove conf file (%s) from expected" % cfile
+        print "  -- root cause: %s" % e
+    shutil.rmtree(rundir, True) # Clean up test directory
+    if not settings.has_key("disable-svn"):
+      ecode = os.spawnlp(os.P_WAIT, svn, svn, "add", expectdir)
+      if ecode != 0: return False
+
+    return True
+    
+  # } // End of cTest::handleNewExpected()
+
+
 
   # bool cTest::wasSuccessful() {
   def wasSuccessful(self): return self.success
   # } // End of cTest::wasSuccessful()
     
 
+
   # void cTest::reportResults() {
   def reportResults(self):
     print "%s :" % self.name, 
     if self.success:
       if self.has_expected: print "passed"
-      else: print "new expected results generated"
+      else:
+        if self.handleNewExpected():
+          print "new expected results generated"
+        else:
+          print "unable to process new expected results"
+          self.success = False
     else:
       print "failed\n"
       if self.exitcode != 0:
@@ -193,6 +254,7 @@ class cTest:
   # } // End of cTest::reportResults()
     
 # } // End of class cTest
+
 
 
 
@@ -208,6 +270,7 @@ def getConfigString(sect, opt, default):
 
 
 
+
 # void usage() {
 def usage():
   global settings
@@ -219,13 +282,31 @@ Usage: testrunner.py [options]
       Display this message
     
     --builddir=[$builddir]
-      Set the path to the build directory
+      Set the path to the build directory.
+    
+    --disable-svn
+      Disable all Subversion usage.
+      
+    --mode=[$mode]
+      Set the test runner mode.  Options are 'local', 'master', and 'slave'.
+      
+      Local mode generates expected results and adds them to the repository,
+      if subversion metadata has been found.  Master mode does the same as
+      local, but also commits the generated expected results automatically.
+      Slave mode disables expected results generation completely.
+
+    -s [$svnpath] | --svnpath=[$svnpath]
+      Set the path to the Subversion command line utility.
+    
+    --svnmetadir=[$svnmetadir]
+      Set the name of the Subversion metadata directory.
     
     --testdir=[$testdir]
-      Set the path to the directory containing tests
+      Set the path to the directory containing tests.
 """)
   print usagestr.substitute(settings)
 # } // End of usage()
+
 
 
 
@@ -240,11 +321,15 @@ def main(argv):
   cfg.read([os.path.join(scriptdir, "testrunner.cfg")])
   
   settings["builddir"] = getConfigString("testrunner", "builddir", "build")
+  settings["mode"] = getConfigString("testrunner", "mode", "local")
+  settings["svnpath"] = getConfigString("testrunner", "svnpath", "svn")
+  settings["svnmetadir"] = getConfigString("testrunner", "svnmetadir", ".svn")
   settings["testdir"] = getConfigString("testrunner", "testdir", "tests")
 
   # Process Command Line Arguments
   try:
-    opts, args = getopt.getopt(argv[1:], "h", ["help", "builddir", "testdir"])
+    opts, args = getopt.getopt(argv[1:], "hm:s:", \
+      ["builddir=", "disable-svn", "help", "mode=", "svnmetadir=", "svnpath=", "testdir="])
   except getopt.GetoptError:
     usage()
     return -1
@@ -255,8 +340,16 @@ def main(argv):
       showhelp = True
     elif opt == "--builddir":
       settings["builddir"] = arg
+    elif opt in ("-m", "--mode"):
+      settings["mode"] = arg
+    elif opt == "--disable-svn":
+      settings["disable-svn"] = ""
+    elif opt == "--svnmetadir":
+      settings["svnmetadir"] = arg
+    elif opt in ("-s", "--svnpath"):
+      settings["svnpath"] = arg
     elif opt == "--testdir":
-      settings["testdir"] = arg
+    settings["testdir"] = arg
       
   # Show help and exit, if requested to do so
   if showhelp:
@@ -280,6 +373,7 @@ def main(argv):
   tests = []
 
   testdir = os.path.abspath(getConfigString("main", "testdir", "."))  
+  settings["testdir"] = testdir
   tlist = dircache.listdir(testdir)
   dircache.annotate(testdir, tlist)
   for test in tlist:
@@ -310,9 +404,6 @@ def main(argv):
   sys.stdout.write("\n\n")
   sys.stdout.flush()
   
-  # Clean up test directory
-  shutil.rmtree(tmpdir, True)
-
   # Report Results
   success = 0
   fail = 0
@@ -320,6 +411,13 @@ def main(argv):
     test.reportResults()
     if test.wasSuccessful(): success += 1
     else: fail += 1
+
+  if os.path.exists(os.path.join(tmpdir, "_svn_tests")) and not settings.has_key("disable-svn"):
+    # TODO - svn commit new expected results
+    pass
+
+  # Clean up test directory
+  shutil.rmtree(tmpdir, True)
     
   if fail == 0:
     print "\nAll tests passed."
@@ -328,6 +426,7 @@ def main(argv):
     print "\n%d of %d tests failed." % (fail, fail + success)
     return fail
 # } // End of main()  
+
 
 
 
